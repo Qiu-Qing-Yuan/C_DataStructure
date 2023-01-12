2f96ceba2d9ac86f346ecc080722ea6d519e3e11
Qiu-Qingyuan <97420436+Qiu-Qingyuan@users.noreply.github.com>
2023-01-12 10:01:20 +0800

    顺序栈完成初始化

---
+++ Stack/SequentialStack/cmake-build-debug/CMakeFiles/Stack.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stack.dir/main.c.obj"
+  "CMakeFiles/Stack.dir/stack.c.obj"
+  "Stack.exe"
+  "Stack.exe.manifest"
+  "Stack.pdb"
+  "libStack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
