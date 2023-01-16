2e2e979908a2b39fe316b4554566da4b649096dc
Qiu-Qingyuan <97420436+Qiu-Qingyuan@users.noreply.github.com>
2023-01-16 10:51:07 +0800

    顺序栈基本操作

---
+++ Stack/LinkStack/cmake-build-debug/CMakeFiles/LinkStack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkStack.dir/main.c.obj"
+  "LinkStack.exe"
+  "LinkStack.exe.manifest"
+  "LinkStack.pdb"
+  "libLinkStack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LinkStack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Stack/LinkStack/cmake-build-debug/CMakeFiles/LinkStack.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LinkStack.
+# This may be replaced when dependencies are built.
