cd565d2b1a6f82dac0c34caf10bca1c2a600b67f
Qiu-Qingyuan <97420436+Qiu-Qingyuan@users.noreply.github.com>
2023-04-27 19:08:02 +0800

    循环队列理解

--- Tree/Tree/common.h
+++ Tree/Tree/common.h
@@ -14,3 +14,23 @@ typedef struct { //树结构
     PTNode nodes[MAX_TREE_SIZE];//结点数组
     int r,n;//根的位置和结点数
 }PTree;
+
+
+
+/*树的孩子表示法结构定义*/
+typedef struct CTNode /*孩子结点*/
+{
+    int child; /*为数据域，存储某个结点在表头数组中的下标*/
+    struct CTNode *next;
+} *ChildPtr;
+
+typedef struct /*表头结构*/
+{
+    TElemType  data;
+    ChildPtr firstChild;
+}CTBox;
+typedef struct /*树结构*/
+{
+    CTBox nodes[MAX_TREE_SIZE]; /*结点数组*/
+    int r,n;/*根的位置和结点数*/
+}CTree;
